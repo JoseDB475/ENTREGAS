7a2f66acc923360b80b80c464a3f7f9fdecf6661
JOSE DUARTE <bmwcasivuela@hotmail.com>
2026-04-28 21:54:06 -0500

    ejercicio cajero

---
+++ en-clase/cajero/index.js
@@ -0,0 +1,7 @@
+// simular un cajero automatico , el cajero puede hacer diversas tareas, crear la cuenta requiere de un saldo de :
+// 1. consignar un dinero: recordar que no se pueden consignar valores negativo
+// 2. retirar: al retirar solo puede retirar dejando un saldo del 10% del saldo de la apertura de la
+// 3. consultar
+// 4. pagar: me va cobrar el 5%
+// 5. trasferir: me cobrara el 1%
+// 6. historial de movimientos
