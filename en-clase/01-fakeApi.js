abad9f8b90bdae2b34b1981a25c14660f2c78bdd
JOSE DUARTE <bmwcasivuela@hotmail.com>
2026-04-28 20:04:56 -0500

    02-fakeApi.js

--- en-clase/01-fakeApi.js
+++ en-clase/01-fakeApi.js
@@ -1,17 +1,60 @@
+// const API = "https://fakeapi.net/products";
+
+// async function getProducts() {
+//     try{
+//         const response = await fetch (API);
+//     const data = await response.json ();
+
+//     console.log (data);
+//     } catch (error){
+//         console.error("error al conectarse al fakeApi");
+//     }
+// }
+    
+// getProducts();
+    
+
+
+
+
+// Fetch API: API JavaScript para realizar peticiones HTTP
 const API = "https://fakeapi.net/products";
 
-async function getProducts() {
-    try{
-        const response = await fetch (API);
-    const data = await response.json ();
+const calculateTotal = ( products ) => {
 
-    console.log (data);
-    } catch (error){
-        console.error("error al conectarse al fakeApi");
+    let total = 1;
+
+    for( let i = 0; i < products.length; i++ ) {
+        total = products[i].price * products[i].stock;
     }
+
+    return total;
+} 
+
+// const calculateTotal = ( products ) => {
+//     let total = 0;
+
+//     for( let i = 0; i < products.length; i++ ) {
+//         total = total + products[i].price;
+//     }
+
+//     return total;
+// } 
+
+// Async / Await:
+async function getProducts() {
+    try {
+        const response = await fetch( API );        // GET: Obtener los datos del Objeto Response (Respuesta)
+        const data = await response.json();      // Obtener los datos y formaterarlos como unobjeto JSON
+    
+        // Implementacion
+        const total = calculateTotal( data.data );
+
+        console.log( `El valor total de ${ total }` ); 
+    } catch (error) {
+        console.error( "Error al conectarse al FakeAPI" );
+    }
+
 }
-    
+
 getProducts();
-    
-
-
---
+++ en-clase/02-fakeApi.js
@@ -0,0 +1,5 @@
+
+// Extraer el listado de productos del siguiente endpoint, y obtener el listado de productos populares.Se considera un producto popular a todo aquel que tenga una calificación(rate), de 4.7 o más, pero que la cantidad de usuarios que hallan votado(count) sea mayor a 100.
+
+const API = "https://fakeapi.net/products?limit=20";
+
