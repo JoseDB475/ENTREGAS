6bbeca39ed86c9f4d0d70e6e0d712f62e50d18cc
JOSE DUARTE <bmwcasivuela@hotmail.com>
2026-04-30 21:17:20 -0500

    9 ejercicio

---
+++ talleres/taller basico/9.ejercicio.js
@@ -0,0 +1,19 @@
+const colors =[];
+
+let color = prompt("ingrese el primer color");
+    colors.push(color);
+    color = prompt("ingrese el segundo color");
+    colors.push(color);
+    color = prompt("ingrese el tercer color ");
+    colors.push(color);
+    
+    console.log(colors);
+
+
+
+
+
+
+    
+
+    
