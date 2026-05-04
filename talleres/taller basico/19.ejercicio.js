14a125edccee32d9e6a51386160186564742d943
JOSE DUARTE <bmwcasivuela@hotmail.com>
2026-05-04 17:19:03 -0500

    19.ejercicio desarrollado

---
+++ talleres/taller basico/19.ejercicio.js
@@ -0,0 +1,13 @@
+const aleatorio = Math.floor(Math.random() * 10) + 1;
+
+
+const intento = prompt("Adivina el número (entre 1 y 10):");
+
+
+const numeroUsuario = Number(intento);
+
+if (numeroUsuario === aleatorio) {
+    alert("¡Correcto! Adivinaste el número 🎉");
+} else {
+    alert("Incorrecto  El número es: " + aleatorio);
+}
