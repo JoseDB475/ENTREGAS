534e4cec5aec3ccfa633a8aa01bf6a2c2dd990ce
JOSE DUARTE <bmwcasivuela@hotmail.com>
2026-05-04 17:10:43 -0500

    17 ejercicio desarrollado

---
+++ talleres/taller basico/17.ejercicio.js
@@ -0,0 +1,11 @@
+const num1 = Number(prompt("ingresa el primer numero"));
+const num2 = Number(prompt("ingresa el segundo numero"));
+
+
+if (num1 > 0) {
+    console.log("num1 es positivo");
+} else if (num1 < 0) {
+    console.log("num1 es negativo");
+} else {
+    console.log("num1 es cero");
+}
