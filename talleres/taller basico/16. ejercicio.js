668ff891afa519d9c57d649e0a1491aa78a0406f
JOSE DUARTE <bmwcasivuela@hotmail.com>
2026-05-04 17:05:10 -0500

    16.ejercio desarrollado

---
+++ talleres/taller basico/16. ejercicio.js
@@ -0,0 +1,17 @@
+// 16.
+const num1 = Number(prompt("ingresa el primer numero"));
+const num2 = Number(prompt("ingresa el segundo numero"));
+
+// const resultado = {};
+
+    if (num1>num2){
+    alert("El numero mayor es ", num1)
+} else if (num2>num1){
+    alert("el numero mayor es ", num2)
+} else {
+    alert("ambos numeros son iguales");
+}
+
+// no me dio, falta correjir
+
+
