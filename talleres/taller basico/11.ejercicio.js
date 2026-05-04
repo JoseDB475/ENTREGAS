ae597e6b78141103bcee7bf740a0a1e1a465a087
JOSE DUARTE <bmwcasivuela@hotmail.com>
2026-05-03 23:27:41 -0500

    desarrollo ejercicio 11

---
+++ talleres/taller basico/11.ejercicio.js
@@ -0,0 +1,20 @@
+// 11. Crea un objeto (persona) con 
+// propiedades: nombre, edad, ciudad.
+// Muestra cada propiedad usando notación
+// de punto (persona.nombre) y notación 
+// de corchetes (persona['edad']).
+
+const persona = {
+    nombre: "gusi",
+    edad: 29,
+    ciudad: "madrid"
+}
+
+console.log(persona.nombre);
+console.log(persona.edad);
+console.log(persona.ciudad);
+console.log(persona[`nombre`]);
+console.log(persona[`edad`]);
+console.log(persona[`ciudad`]);
+
+
