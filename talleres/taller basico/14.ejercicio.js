58c065d466d20897509769918dc1a30a2624d3c6
JOSE DUARTE <bmwcasivuela@hotmail.com>
2026-05-04 16:47:42 -0500

    14 ejercicio desarrollado

---
+++ talleres/taller basico/14.ejercicio.js
@@ -0,0 +1,11 @@
+// 14.
+
+const nombre = prompt("ingresa tu nombre");
+const edad = prompt("ingresa tu edad");
+
+const usuario = {
+    nombre:nombre,
+    edad: Number(edad)
+}
+
+console.log(usuario);
