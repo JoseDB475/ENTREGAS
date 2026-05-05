7fb970c4ca78837a41eed84b8bfe243fed0ec9b6
JOSE DUARTE <bmwcasivuela@hotmail.com>
2026-05-04 19:16:29 -0500

    20 ejercicio desarrollado

--- talleres/taller basico/19.ejercicio.js
+++ talleres/taller basico/19.ejercicio.js
@@ -7,7 +7,7 @@ const intento = prompt("Adivina el número (entre 1 y 10):");
 const numeroUsuario = Number(intento);
 
 if (numeroUsuario === aleatorio) {
-    alert("¡Correcto! Adivinaste el número 🎉");
+    alert("Correcto, Adivinaste el número ");
 } else {
     alert("Incorrecto  El número es: " + aleatorio);
 }
---
+++ talleres/taller basico/20.ejercicio.js
@@ -0,0 +1,19 @@
+// 20.
+
+const num = Number(prompt("ingresa un numero"));
+
+console.log(num);
+
+
+
+ if  (num % 2 === 0) {
+
+    console.log("El numero es par")
+    
+
+}else {
+    console.log("Este numero es impar")
+}
+
+
+ 
