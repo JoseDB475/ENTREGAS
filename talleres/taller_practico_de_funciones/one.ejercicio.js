9a0952d6bedfdbe6c507d5f76f577d20d550b39a
JOSE DUARTE <bmwcasivuela@hotmail.com>
2026-05-05 18:29:13 -0500

    primer ejercicio del segundo taller

---
+++ talleres/taller_practico_de_funciones/one.ejercicio.js
@@ -0,0 +1,10 @@
+// 1. Sumar elementos de un array:
+// Crea una función sumarArray(array)
+// que reciba un array de números y devuelva la suma.
+// Ejemplo: sumarArray([1, 2, 3]) → 6.
+
+function sumarArray (numeros){
+    
+};
+
+const numeros = [7,75,97,103,151];
